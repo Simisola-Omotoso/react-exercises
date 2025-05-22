219d994d66b973a5060ef850a7540159ec9c71d6
Simi0M0 <somotoso@Outlook.com>
2025-05-21 22:15:27 -0400

    Child to Parent Components
    practiced this aspect inside App.jsx and Child.jsx

--- React-Exercises/src/Child.jsx
+++ React-Exercises/src/Child.jsx
@@ -1,5 +1,14 @@
 function Child(props) {
-    return <p>{props.message}</p>;
+    function sendMessage() {
+        props.onSend("I go to University of West Georgia.");
+    }
+
+    return (
+        <div>
+            <p>{props.message}</p>
+            <button onClick={sendMessage}>Send Message</button>
+        </div>
+    );
 }
 
 export default Child;
